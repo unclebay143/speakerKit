3dd16e79e5e1c0147d2ef431801ff0a6a5788204
Seun Salau <marrizzsalau7@gmail.com>
2025-07-09 08:21:11 +0100

    fixing build error

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
-import User from "@/models/User";
 import connectViaMongoose from "@/lib/db";
 import bcrypt from "bcryptjs";
+import User from "@/models/User";
 
 export async function POST(req: Request) {
   try {
